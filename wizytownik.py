4f62f8a858cb191fcf76ce8119aed3865004fc08
Dorota Pietruszka <do.pietruszka@gmail.com>
2022-04-20 10:41:01 +0200

    language correction

--- wizytownik.py
+++ wizytownik.py
@@ -1,4 +1,4 @@
-class Wizytowka:
+class Cardholder:
     def __init__(self, name, surname, company, position, mail):
         self.name = name
         self.surname = surname
@@ -6,15 +6,15 @@ class Wizytowka:
         self.position = position
         self.mail = mail
 
-Anna = Wizytowka(name="Anna", surname="Kowalska", company="A", position="ceo", mail="anna.kowalska@mail.pl")
-Aleksandra = Wizytowka(name="Aleksandra", surname="Nowak", company="B", position="section manager", mail="aleksandra.nowak@mail.pl")
-Olga = Wizytowka(name="Olga", surname="Wiśniewska", company="C", position="assistant", mail="olga.wisniewska@mail.pl")
-Jan = Wizytowka(name="Jan", surname="Wójcik", company="D", position="HR", mail="jan.wojcik@mail.pl")
-Adam = Wizytowka(name="Adam", surname="Kowalczyk", company="E", position="main dev", mail="adam.kowalczyk@mail.pl")
+anna = Cardholder(name="Anna", surname="Kowalska", company="A", position="ceo", mail="anna.kowalska@mail.pl")
+aleksandra = Cardholder(name="Aleksandra", surname="Nowak", company="B", position="section manager", mail="aleksandra.nowak@mail.pl")
+olga = Cardholder(name="Olga", surname="Wiśniewska", company="C", position="assistant", mail="olga.wisniewska@mail.pl")
+jan = Cardholder(name="Jan", surname="Wójcik", company="D", position="HR", mail="jan.wojcik@mail.pl")
+adam = Cardholder(name="Adam", surname="Kowalczyk", company="E", position="main dev", mail="adam.kowalczyk@mail.pl")
 
-wizytownik = [Anna, Aleksandra, Olga, Jan, Adam]
+cardholder = [anna, aleksandra, olga, jan, adam]
 
-print(wizytownik)
+print(cardholder)
 
-for item in wizytownik:
+for item in cardholder:
     print(item.name, item.surname, item.company, item.position, item.mail)
